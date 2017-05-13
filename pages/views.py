c496aafa54983dd87461118a57a4e68580d8f408
Helio Correia <helio.as.correia@gmail.com>
2017-05-13 13:03:00 +0100

    Show articles in home page

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from blog.models import Article
+
 
 def home(request):
-    return render(request, 'pages/home.html', {})
+    articles = Article.objects.all()
+    return render(request, 'pages/home.html', {'articles': articles})
