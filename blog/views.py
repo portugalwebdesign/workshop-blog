7156d0469919ce6a28f79dabac591f7b58913312
Helio Correia <helio.as.correia@gmail.com>
2017-06-03 16:24:18 +0100

    Add comments to detail page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
-from blog.models import Article
+from .forms import CommentForm
+from .models import Article
 
 
 def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
-    return render(request, 'blog/article/detail.html', {'article': article})
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment_form.save()
+            return redirect(request.META.get('HTTP_REFERER'))
+    else:
+        comment_form = CommentForm(initial={'article': article})
+
+    return render(request, 'blog/article/detail.html', {
+        'article': article,
+        'comment_form': comment_form
+    })
